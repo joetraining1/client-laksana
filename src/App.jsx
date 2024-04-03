fda9f9b9fcd13baf07e1af1df2a47e042b0d6953
joetraining1 <josephbagussw@gmail.com>
2024-04-03 22:19:39 +0700

    adding first routed page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Home from "./pages/Home"
+import { Routes, Route } from "react-router-dom"
+
 function App() {
 
   return (
-    <div>Hello</div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    console.log('hadir')
+  return (
+    <div style={{
+        height: '100svh',
+        width: '100vw'
+    }}>
+      Hellow
+    </div>
+  )
+}
+
+export default Home
